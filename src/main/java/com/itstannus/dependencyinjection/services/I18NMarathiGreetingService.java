b59f11084ff55db97a6331a18965285e18b64806
tanvishinde92@gmail.com <tshinde@CGLX-LPT-168U>
2021-04-22 15:31:29 +0530

    default profile example

--- src/main/java/com/itstannus/dependencyinjection/services/I18NMarathiGreetingService.java
+++ src/main/java/com/itstannus/dependencyinjection/services/I18NMarathiGreetingService.java
@@ -3,7 +3,7 @@ package com.itstannus.dependencyinjection.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("MR")
+@Profile({"MR","HN","default"})
 @Service("i18nService")
 public class I18NMarathiGreetingService implements GreetingService {
 
